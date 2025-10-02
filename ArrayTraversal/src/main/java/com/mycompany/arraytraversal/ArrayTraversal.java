4208e7b60d4d8ec2ec3b928425b42c16186613db
Bakhtawar29580 <kbakhtawar341@gmail.com>
2025-10-02 19:32:00 +0500

    traverse

---
+++ ArrayTraversal/src/main/java/com/mycompany/arraytraversal/ArrayTraversal.java
@@ -0,0 +1,13 @@
+package com.mycompany.arraytraversal;
+
+public class ArrayTraversal {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20, 25, 30};
+
+        System.out.println("Array elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
