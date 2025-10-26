ed35e18a7027e581f00f7588c9fcae91715afbae
Bakhtawar29580 <kbakhtawar341@gmail.com>
2025-10-26 12:34:38 +0500

    add

---
+++ add/src/main/java/com/mycompany/add/Add.java
@@ -0,0 +1,11 @@
+package com.mycompany.add;
+
+public class Add {
+
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 10;
+        int add = num1 + num2;
+        System.out.println("Additing " + num1 + " and " + num2 + " gives: " + add);
+    }
+}
