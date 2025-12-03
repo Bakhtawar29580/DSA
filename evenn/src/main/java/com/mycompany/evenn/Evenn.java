53463783f0b10c879b3de00ddb49f9578d65e6c9
Bakhtawar29580 <kbakhtawar341@gmail.com>
2025-12-04 00:07:31 +0500

    even

---
+++ evenn/src/main/java/com/mycompany/evenn/Evenn.java
@@ -0,0 +1,16 @@
+package com.mycompany.evenn;
+
+public class Evenn {
+
+    public static void main(String[] args) {
+        int num1 = 8;
+        
+        if(num1 %2 == 0){
+            System.out.println("The number " + num1 + " is even!");
+        }
+        else{
+            System.out.println("The number " + num1 + " is odd!");
+        }
+        
+    }
+}
